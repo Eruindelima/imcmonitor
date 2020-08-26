8b40bb3f77bf6ec917f99a64fbe9d81154ab70f9
Eruin de Lima Silva <eruin.silva@gmail.com>
2020-08-26 00:45:19 +0000

    inclusao do readme e de testes (basico)

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ class Main {
   public static void main(String[] args) {
     
 
+    TesteSimples teste = new TesteSimples();
+    teste.executar(); 
+
     System.out.println("imcmonitor, Fim!");
   }
 }
---
+++ TesteSimples.java
@@ -0,0 +1,12 @@
+class TesteSimples {
+
+  public executar(){
+
+    Atleta atleta = new atleta();
+    atleta.nome = "Piedade Silva";
+    atleta.idade = 36;
+
+    System.out.println("Nome = " + atleta.nome);
+    System.out.println("Idade = " + atleta.idade);
+  }
+}
